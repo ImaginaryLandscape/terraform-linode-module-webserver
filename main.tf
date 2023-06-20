d519cb7f6f1524d7aafa5007ba6a6189c5684821
Eprem Zadykyan <e.zadykyan.anthill@gmail.com>
2023-06-20 13:33:03 +0300

    update hostname

--- main.tf
+++ main.tf
@@ -44,11 +44,23 @@ resource "linode_instance" "web" {
     destination = "/tmp/access_setup.sh"
   }
 
+    provisioner "file" {
+    source      = "user.txt"
+    destination = "/tmp/user.txt"
+  }
+
+   provisioner "file" {
+    source      = "useradd.sh"
+    destination = "/tmp/useradd.sh"
+  }
+
   provisioner "remote-exec" {
     inline = [
       "sudo chmod +x /tmp/access_setup.sh",
       "sudo sh /tmp/access_setup.sh -u ${var.admin_user} -k '${local.ssh_keys_str}'",
       "sudo bash -c \"echo '${var.admin_user}:${random_string.password.result}' | sudo chpasswd\"",
+      "service sshd restart",
+      "sudo hostnamectl set-hostname '${var.SITE}-web${var.ID + count.index}.${var.DOMAIN}'" 
     ]
   }
 
